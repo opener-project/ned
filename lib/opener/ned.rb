4a35f8624ef714a02f78ace2dbc2386b43e68fce
Yorick Peterse <yorickpeterse@olery.com>
2013-06-18 14:33:00 +0200

    Ruby interface for the NED kernel.
    Signed-off-by: Yorick Peterse <yorickpeterse@olery.com>

--- lib/opener/ned.rb
+++ lib/opener/ned.rb
@@ -1,1 +1,108 @@
+require 'open3'
+require 'optparse'
+
 require_relative 'ned/version'
+require_relative 'ned/cli'
+
+module Opener
+  ##
+  # Ruby wrapper around the Java based NED tool that's powered by DBpedia.
+  #
+  # @!attribute [r] options
+  #  @return [Hash]
+  #
+  class NED
+    attr_reader :options
+
+    ##
+    # @return [String]
+    #
+    DEFAULT_HOST = 'localhost'
+
+    ##
+    # @return [Numeric]
+    #
+    DEFAULT_PORT = 2020 # English
+
+    ##
+    # Hash containing the default options to use.
+    #
+    # @return [Hash]
+    #
+    DEFAULT_OPTIONS = {
+      :args => [],
+      :port => DEFAULT_PORT,
+      :host => DEFAULT_HOST
+    }.freeze
+
+    ##
+    # @param [Hash] options
+    #
+    # @option options [Array] :args Arbitrary arguments to pass to the
+    #  underlying kernel.
+    # @option options [String|Numeric] :port The port number to connect to.
+    # @option options [String] :host The hostname of the DBpedia server.
+    #
+    def initialize(options = {})
+      @options = DEFAULT_OPTIONS.merge(options)
+    end
+
+    ##
+    # Builds the command to use for executing the kernel.
+    #
+    # @return [String]
+    #
+    def command
+      return "java -jar #{kernel} #{command_arguments.join(' ')}"
+    end
+
+    ##
+    # Processes the input and returns an Array containing the output of STDOUT,
+    # STDERR and an object containing process information.
+    #
+    # @param [String] input The text of which to detect the language.
+    # @return [Array]
+    #
+    def run(input)
+      if !input or input.strip.empty?
+        raise ArgumentError, 'No input specified'
+      end
+
+      unless File.file?(kernel)
+        raise "The Java kernel (#{kernel}) does not exist"
+      end
+
+      return Open3.capture3(command, :stdin_data => input)
+    end
+
+    protected
+
+    ##
+    # Returns the arguments to pass to the underlying kernel as an Array.
+    #
+    # @return [Array]
+    #
+    def command_arguments
+      arguments = [
+        "-p #{options[:port]}",
+        "-H #{options[:host]}"
+      ]
+
+      return arguments
+    end
+
+    ##
+    # @return [String]
+    #
+    def core_dir
+      return File.expand_path('../../../core/target', __FILE__)
+    end
+
+    ##
+    # @return [String]
+    #
+    def kernel
+      return File.join(core_dir, 'ehu-ned-1.0.jar')
+    end
+  end # NED
+end # Opener
---
+++ lib/opener/ned/cli.rb
@@ -0,0 +1,100 @@
+module Opener
+  class NED
+    ##
+    # CLI wrapper around {Opener::NED} using OptionParser.
+    #
+    # @!attribute [r] options
+    #  @return [Hash]
+    # @!attribute [r] option_parser
+    #  @return [OptionParser]
+    #
+    class CLI
+      attr_reader :options, :option_parser
+
+      ##
+      # @param [Hash] options
+      #
+      def initialize(options = {})
+        @options = DEFAULT_OPTIONS.merge(options)
+
+        @option_parser = OptionParser.new do |opts|
+          opts.program_name   = 'ned'
+          opts.summary_indent = '  '
+
+          opts.on('-h', '--help', 'Shows this help message') do
+            show_help
+          end
+
+          opts.on('-v', '--version', 'Shows the current version') do
+            show_version
+          end
+
+          opts.on('-p', '--port [VALUE]', 'Use a custom port') do |value|
+            @options[:port] = value
+          end
+
+          opts.on('-H', '--host [VALUE]', 'Use a custom hostname') do |value|
+            @options[:host] = value
+          end
+
+          opts.separator <<-EOF
+
+Examples:
+
+  cat input_file.kaf | #{opts.program_name}
+  cat input_file.kaf | #{opts.program_name} --host=http://some-host.com/
+
+Port Numbers:
+
+  Port numbers are required. Each language has its own port number (unless
+  specified otherwise):
+
+  * German: 2010
+  * English: 2020
+  * Spanish: 2030
+  * French: 2040
+  * Italian: 2050
+  * Dutch: 2060
+
+  By default port 2020 (English) is used.
+          EOF
+        end
+      end
+
+      ##
+      # @param [String] input
+      #
+      def run(input)
+        option_parser.parse!(options[:args])
+
+        ned = NED.new(options)
+
+        stdout, stderr, process = ned.run(input)
+
+        if process.success?
+          puts stdout
+
+          STDERR.puts(stderr) unless stderr.empty?
+        else
+          abort stderr
+        end
+      end
+
+      private
+
+      ##
+      # Shows the help message and exits the program.
+      #
+      def show_help
+        abort option_parser.to_s
+      end
+
+      ##
+      # Shows the version and exits the program.
+      #
+      def show_version
+        abort "#{option_parser.program_name} v#{VERSION} on #{RUBY_DESCRIPTION}"
+      end
+    end # CLI
+  end # NED
+end # Opener
