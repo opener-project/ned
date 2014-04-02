e049198f419006efa87fd88512f557da713ac7b0
Wilco van Duinkerken <wilco@sparkboxx.com>
2014-04-02 15:10:03 +0200

    overload annotate and extract functions to use full endpoint.

--- core/src/main/java/ehu/ned/Annotate.java
+++ core/src/main/java/ehu/ned/Annotate.java
@@ -23,138 +23,157 @@ import org.w3c.dom.Element;
 
 public class Annotate {
 
-    DBpediaSpotlightClient c;
-    public Annotate(){
-	c = new DBpediaSpotlightClient ();
+  DBpediaSpotlightClient c;
+
+  public Annotate(){
+    c = new DBpediaSpotlightClient ();
+  }
+
+  public void disambiguateNEsToKAF (KAFDocument kaf, String endpoint) throws Exception {
+    addLinguisticProcessor(kaf);
+    String annotation = KAF2XMLSpot(kaf);
+
+    Document response = annotate(annotation, endpoint);
+    XMLSpot2KAF(kaf,response);
+  }
+
+  public void disambiguateNEsToKAF (KAFDocument kaf, String host, String port) throws Exception {
+    addLinguisticProcessor(kaf);
+    String annotation = KAF2XMLSpot(kaf);
+
+    Document response = annotate(annotation, host, port);
+    XMLSpot2KAF(kaf,response);
+  }
+
+  public void addLinguisticProcessor(KAFDocument kaf) {
+    kaf.addLinguisticProcessor("ehu-ned", "ehu-dbpedia-spotlight", "1.0");
+  }
+
+  private String KAF2XMLSpot(KAFDocument kaf){
+    /*
+       <annotation text="Brazilian oil giant Petrobras and U.S. oilfield service company Halliburton have signed a technological cooperation agreement, Petrobras announced Monday. The two companies agreed on three projects: studies on contamination of fluids in oil wells, laboratory simulation of well production, and research on solidification of salt and carbon dioxide formations, said Petrobras. Twelve other projects are still under negotiation.">
+       <surfaceForm name="oil" offset="10"/>
+       <surfaceForm name="company" offset="56"/>
+       <surfaceForm name="Halliburton" offset="64"/>
+       <surfaceForm name="oil" offset="237"/>
+       <surfaceForm name="other" offset="383"/>
+       </annotation>
+       */
+
+    String text = "";
+    List<List<WF>> sentences = kaf.getSentences();
+    for (List<WF> sentence : sentences) {
+      for (int i = 0; i < sentence.size(); i++) {
+        if (!text.isEmpty()) {
+          text += " ";
+        }
+        String tok = sentence.get(i).getForm();
+        if (tok.contains("\"")){
+          tok = tok.replaceAll("\"","'");
+        }
+        if (tok.matches("&")) {
+          tok = tok.replaceAll("&","amper");
+        }
+        text += tok;
+      }
     }
 
-    public void disambiguateNEsToKAF (KAFDocument kaf, String host, String port) throws Exception { 
-	kaf.addLinguisticProcessor("ehu-ned", "ehu-dbpedia-spotlight", "1.0");
-	String annotation = KAF2XMLSpot(kaf);
-	Document response = annotate(annotation, host, port);
-	XMLSpot2KAF(kaf,response);
+    int offset = -1;
+    String entStr = "";
+    String forms = "";
+    List<Entity> entities = kaf.getEntities();
+    for (Entity entity : entities){
+      List<List<Term>> references = entity.getReferences();
+      for (List<Term> ref : references){
+        offset = -1;
+        entStr = "";
+        for (Term t: ref){
+          List<WF> words = t.getWFs();
+          for (int i = 0; i < words.size(); i++){
+            if (!entStr.isEmpty()){
+              entStr += " ";
+            }
+            WF word = words.get(i);
+            entStr += word.getForm();
+            if (offset == -1){
+              if (word.hasOffset()){
+                offset = word.getOffset();
+              }
+              else{
+                System.out.println("There is not offset for word id " + word.getId());
+              }
+            }
+          }
+        }
+      }
+      // Each reference is a spot to disambiguate
+      forms += "<surfaceForm name=\"" + entStr + "\" offset=\"" + offset + "\"/>\n";
     }
 
-    private String KAF2XMLSpot(KAFDocument kaf){ 
-	/*
-	  <annotation text="Brazilian oil giant Petrobras and U.S. oilfield service company Halliburton have signed a technological cooperation agreement, Petrobras announced Monday. The two companies agreed on three projects: studies on contamination of fluids in oil wells, laboratory simulation of well production, and research on solidification of salt and carbon dioxide formations, said Petrobras. Twelve other projects are still under negotiation.">
-	  <surfaceForm name="oil" offset="10"/>
-	  <surfaceForm name="company" offset="56"/>
-	  <surfaceForm name="Halliburton" offset="64"/>
-	  <surfaceForm name="oil" offset="237"/>
-	  <surfaceForm name="other" offset="383"/>
-	  </annotation>
-	*/
-	
-	String text = "";
-	List<List<WF>> sentences = kaf.getSentences();
-	for (List<WF> sentence : sentences) {
-	    for (int i = 0; i < sentence.size(); i++) {
-		if (!text.isEmpty()) {
-		    text += " ";
-		}
-		String tok = sentence.get(i).getForm();
-		if (tok.contains("\"")){
-		    tok = tok.replaceAll("\"","'");
-		}
-		 if (tok.matches("&")) { 
-	        	tok = tok.replaceAll("&","amper");
-	    }
-		text += tok;
-	    }
-	}
-	
-	int offset = -1;
-	String entStr = "";
-	String forms = "";
-	List<Entity> entities = kaf.getEntities();
-	for (Entity entity : entities){
-	    List<List<Term>> references = entity.getReferences();
-	    for (List<Term> ref : references){
-		offset = -1;
-		entStr = "";
-		for (Term t: ref){
-		    List<WF> words = t.getWFs();
-		    for (int i = 0; i < words.size(); i++){
-			if (!entStr.isEmpty()){
-			    entStr += " ";
-			}
-			WF word = words.get(i);
-			entStr += word.getForm();
-			if (offset == -1){
-			    if (word.hasOffset()){
-				offset = word.getOffset();
-			    }
-			    else{
-				System.out.println("There is not offset for word id " + word.getId());
-			    }
-			}
-		    }
-		}
-	    }
-	    // Each reference is a spot to disambiguate
-	    forms += "<surfaceForm name=\"" + entStr + "\" offset=\"" + offset + "\"/>\n";
-	}
-	
-	String annotation = "<annotation text=\"" + text + "\">\n" + forms + "</annotation>";
-	return annotation;
+    String annotation = "<annotation text=\"" + text + "\">\n" + forms + "</annotation>";
+    return annotation;
+  }
+
+  private Document annotate(String annotation, String endpoint) throws AnnotationException {
+    Document response = c.extract(new Text(annotation), endpoint);
+    return response;
+  }
+
+  private Document annotate(String annotation, String host, String port) throws AnnotationException {
+    Document response = c.extract(new Text(annotation), host, port);
+    return response;
+  }
+
+  private void XMLSpot2KAF(KAFDocument kaf, Document doc){
+    // Store the References into a hash. Key: offset
+    HashMap<Integer,String> refs = new HashMap<Integer,String>();
+
+
+    doc.getDocumentElement().normalize();
+    NodeList nList = doc.getElementsByTagName("Resource");
+
+    for (int temp = 0; temp < nList.getLength(); temp++) {
+
+      Node nNode = nList.item(temp);
+
+      if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+        Element eElement = (Element) nNode;
+        refs.put(new Integer(eElement.getAttribute("offset")),eElement.getAttribute("URI"));
+
+      }
     }
 
-    private Document annotate(String annotation, String host, String port) throws AnnotationException {
-	Document response = c.extract(new Text(annotation), host, port);
-	return response;
+
+    String resource = "spotlight_v1";
+    List<Entity> entities = kaf.getEntities();
+    for (Entity entity : entities){
+      // Get the offset of the entity
+      int offset = getEntityOffset(entity);
+      if (refs.containsKey(offset)){
+        String reference = refs.get(offset);
+        // Create ExternalRef
+        ExternalRef externalRef = kaf.createExternalRef(resource,reference);
+        // addExternalRef to Entity
+        entity.addExternalRef(externalRef);
+      }
     }
-    
-    private void XMLSpot2KAF(KAFDocument kaf, Document doc){
-	// Store the References into a hash. Key: offset
-	HashMap<Integer,String> refs = new HashMap<Integer,String>();
+  }
 
-
-	doc.getDocumentElement().normalize();
-	NodeList nList = doc.getElementsByTagName("Resource");
-		    
-	for (int temp = 0; temp < nList.getLength(); temp++) {
-	    
-	    Node nNode = nList.item(temp);
-	    
-	    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-		
-		Element eElement = (Element) nNode;
-		refs.put(new Integer(eElement.getAttribute("offset")),eElement.getAttribute("URI"));		
-
-	    }
-	}
-	
-
-	String resource = "spotlight_v1";
-	List<Entity> entities = kaf.getEntities();
-	for (Entity entity : entities){
-	    // Get the offset of the entity
-	    int offset = getEntityOffset(entity);
-	    if (refs.containsKey(offset)){
-		String reference = refs.get(offset);
-		// Create ExternalRef
-		ExternalRef externalRef = kaf.createExternalRef(resource,reference);	    
-		// addExternalRef to Entity
-		entity.addExternalRef(externalRef);
-	    }
-	}
+  private int getEntityOffset(Entity ent){
+    int offset = -1;
+    List<List<Term>> references = ent.getReferences();
+    for (List<Term> ref : references){
+      for (Term t: ref){
+        List<WF> words = t.getWFs();
+        for (int i = 0; i < words.size(); i++){
+          WF word = words.get(i);
+          if (word.hasOffset()){
+            return word.getOffset();
+          }
+        }
+      }
     }
-
-    private int getEntityOffset(Entity ent){
-	int offset = -1;
-	List<List<Term>> references = ent.getReferences();
-	for (List<Term> ref : references){
-	    for (Term t: ref){
-		List<WF> words = t.getWFs();
-		for (int i = 0; i < words.size(); i++){
-		    WF word = words.get(i);
-		    if (word.hasOffset()){
-			return word.getOffset();
-		    }
-		}
-	    }
-	}
-	return offset;
-    }
+    return offset;
+  }
 }
--- core/src/main/java/ehu/ned/DBpediaSpotlightClient.java
+++ core/src/main/java/ehu/ned/DBpediaSpotlightClient.java
@@ -33,8 +33,8 @@ import java.io.*;
 //import java.net.URLEncoder;
 
 import org.w3c.dom.Document;
-import javax.xml.parsers.DocumentBuilderFactory; 
-import javax.xml.parsers.DocumentBuilder; 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
 import java.io.StringReader;
 import org.xml.sax.InputSource;
 
@@ -47,7 +47,7 @@ import org.apache.log4j.Logger;
  */
 
 public class DBpediaSpotlightClient {
-    public Logger LOG = Logger.getLogger(this.getClass()); 
+    public Logger LOG = Logger.getLogger(this.getClass());
 
     private static final double CONFIDENCE = 0.0;
     private static final int SUPPORT = 0;
@@ -123,30 +123,40 @@ Petrobras" offset="365"/><surfaceForm name="Twelve" offset="376"/>
 offset="414"/></annotation>
      */
 
-    public Document extract(Text text, String host, String port) throws AnnotationException{
+    public Document extract(Text text, String endpoint) throws AnnotationException {
+     return extract_with_endpoint(text, endpoint);
+    }
 
-        LOG.info("Querying API.");
-		String spotlightResponse = "";
-		Document doc = null;
-		try {
-		    String url = host + ":" + port +"/rest/disambiguate";
-		    PostMethod method = new PostMethod(url);
-		    method.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
-		    NameValuePair[] params = {new NameValuePair("text",text.text()), new NameValuePair("spotter","SpotXmlParser"), new NameValuePair("confidence",Double.toString(CONFIDENCE)), new NameValuePair("support",Integer.toString(SUPPORT))};
-		    method.setRequestBody(params);
-		    method.setRequestHeader(new Header("Accept", "text/xml"));
-		    spotlightResponse = request(method);
-		    doc = loadXMLFromString(spotlightResponse);
-		}
-		catch (javax.xml.parsers.ParserConfigurationException ex) {
-		} 
-		catch (org.xml.sax.SAXException ex) {
-		} 
-		catch (java.io.IOException ex) {
-		} 		
-		
-		return doc;
-	}
+    public Document extract(Text text, String host, String port) throws AnnotationException{
+        String endpoint = host + ":" + port +"/rest/disambiguate";
+        return extract_with_endpoint(text, endpoint);
+    }
+
+    public Document extract_with_endpoint(Text text, String endpoint) throws AnnotationException {
+      LOG.info("Querying API at: "+endpoint);
+
+      String spotlightResponse = "";
+      Document doc = null;
+      try {
+          PostMethod method = new PostMethod(endpoint);
+          method.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
+          NameValuePair[] params = {new NameValuePair("text",text.text()), new NameValuePair("spotter","SpotXmlParser"), new NameValuePair("confidence",Double.toString(CONFIDENCE)), new NameValuePair("support",Integer.toString(SUPPORT))};
+          method.setRequestBody(params);
+          method.setRequestHeader(new Header("Accept", "text/xml"));
+          spotlightResponse = request(method);
+          doc = loadXMLFromString(spotlightResponse);
+      }
+      catch (javax.xml.parsers.ParserConfigurationException ex) {
+      }
+      catch (org.xml.sax.SAXException ex) {
+      }
+      catch (java.io.IOException ex) {
+      }
+
+      return doc;
+
+    }
+
 
     public static Document loadXMLFromString(String xml)  throws org.xml.sax.SAXException, java.io.IOException, javax.xml.parsers.ParserConfigurationException
     {
